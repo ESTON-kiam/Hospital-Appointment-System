eff1838658d94a905205ce6d24f97be68842279e
ESTON-kiam <engestonbrandonkiama@gmail.com>
2024-10-22 13:10:26 +0300

    Appointment System

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+
+    
+    <style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
+  color: white;
+  text-align: center;
+}
+</style>
+
+<div class="footer">
+       <p>&copy; 2021 Appoinment Booking System. Developed by : <a target="_blank" href="">Eston</a>
+</p>
+    </div>
+       
